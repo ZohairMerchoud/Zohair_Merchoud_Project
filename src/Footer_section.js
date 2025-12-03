566fac1d5ba5823beb33ef9ab189336e36550cd9
ZohairMerchoud <merchoudzohair@gmail.com>
2025-12-03 15:52:41 +0100

    Update Footer_section.js

--- src/Footer_section.js
+++ src/Footer_section.js
@@ -6,7 +6,7 @@ const footerStyle = {
   bottom: 0,
   left: 0,
   width: '100%',
-  height: '20vh', // Estimation de la hauteur pour la zone inférieure
+  height: '20vh', 
   overflow: 'hidden',
   zIndex: 0,
 };
@@ -17,7 +17,7 @@ const mountainShapeStyle = {
   bottom: 0,
   left: 0,
   width: '100%',
-  height: '150%', // Permet de dessiner des pointes qui sortent du conteneur
+  height: '150%', 
   backgroundColor: '#0070e0',
   clipPath: 'polygon(0% 100%, 15% 40%, 30% 90%, 45% 30%, 60% 80%, 75% 10%, 90% 100%, 100% 100%)',
   // Ce clip-path simule les pics de montagne vus sur l'image
@@ -26,18 +26,18 @@ const mountainShapeStyle = {
 
 const starStyle = {
   position: 'absolute',
-  bottom: '5vh', // Position estimée
-  left: '45vw', // Centre l'étoile horizontalement
+  bottom: '5vh',
+  left: '45vw', 
   fontSize: '2rem',
-  color: '#333', // Couleur foncée
-  transform: 'rotate(45deg)', // Légère rotation si nécessaire
+  color: '#333', 
+  transform: 'rotate(45deg)', 
   zIndex: 1,
 };
 
 const FooterGraphics = () => (
   <div style={footerStyle}>
     <div style={mountainShapeStyle}></div>
-    <div style={starStyle}>*</div> {/* L'astérisque représente l'étoile */}
+    <div style={starStyle}>*</div> 
   </div>
 );
 
