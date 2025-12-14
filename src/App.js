7b08ed2b7ae93d4635cc780186a0a69dd3ebd736
ZohairMerchoud <merchoudzohair@gmail.com>
2025-12-15 00:06:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import EtudiaLanding from './components/EtudiaLanding';
-import EtudiaSelection from './components/EtudiaSelection';
-import EtudiaModules from './components/EtudiaModules';
-
+import React, { useState } from "react";
+import EtudiaLanding from "./components/EtudiaLanding";
+import EtudiaSelection from "./components/EtudiaSelection";
+import EtudiaModules from "./components/EtudiaModules";
 
 function App() {
   const [selection, setSelection] = useState(null);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<EtudiaLanding />} />
-        <Route 
-          path="/selection" 
-          element={<EtudiaSelection onContinue={setSelection} />} 
-        />
-        <Route 
-          path="/modules" 
-          element={<EtudiaModules selection={selection} />} 
-        />
-      </Routes>
-    </Router>
+    <div>
+      <EtudiaLanding />
+
+      <EtudiaSelection onContinue={setSelection} />
+
+      <EtudiaModules selection={selection} />
+    </div>
   );
 }
 
